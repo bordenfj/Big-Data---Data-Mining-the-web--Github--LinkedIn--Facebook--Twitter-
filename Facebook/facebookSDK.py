443a5e3b6a58baf07d8177316b77d873cda645b7
Aditya Bansal <maddymanu@hotmail.com>
2014-08-06 12:32:21 +0530

    FB Request work

--- Facebook/facebookSDK.py
+++ Facebook/facebookSDK.py
@@ -1,7 +1,6 @@
 import facebook
 import json
 
-
 ACCESS_TOKEN= 'CAACEdEose0cBAAxzznKQRj5yRhwvSvvBKh5dVKOjspQzeJqhZCWcjFKVvOe7rrukGOKqoG72kABtBjEsWSlPPYybZCW0aeo2l1a6GEc5AUEyZBlUOXJlxZAPKVZBLboC8HqddUvb8rrNFqtorL2l9tpKwvr2QCmw2jlSeb1sDAGuq5ZB5ZB83aGbkJRTZAvnc7ebw7MCKp16D6VdZC07113Pr'
 
 # simple function to pretty print some json objects
@@ -9,6 +8,8 @@ def pp(o):
   print json.dumps(o, indent=1)
 
 g = facebook.GraphAPI(ACCESS_TOKEN)
+
+mtsw_id = '146803958708175'
 
 print '---------------'
 print 'Me'
@@ -20,3 +21,10 @@ print '---------------'
 print 'My Friends'
 print '---------------'
 pp(g.get_connections('me' , 'friends'))
+
+print
+print '---------------'
+print 'Social Web'
+print '---------------'
+# pp(g.request("search" , {'q':'social web' , 'type':'page'}))
+pp(g.get_object(mtsw_id))
